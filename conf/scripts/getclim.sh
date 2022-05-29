502cee0b2596f9a68397da3a8cde37922ae4d384
sh <hludyk@gmail.com>
2022-05-29 12:21:07 +0000

    updates

--- conf/scripts/getclim.sh
+++ conf/scripts/getclim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-netcat 192.168.15.248 12345 | while read line
+timeout 5s netcat 192.168.15.248 12345 | while read line
  do
      match=$(echo $line | grep -c '^C')
      if [ $match -eq 1 ]; then
