abb6a7620ca851784ca3a62c91041a2f1d925eee
sviatoslavhludyk <sv.hludyk@gmail.com>
2023-02-26 13:41:45 +0200

    update

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ USERS_FILE_PATH = 'users.json'
 HISTORY_FILE_PATH = 'history.log'
 
 logged_user = None
-#clear = lambda: os.system('clear')
-
 
 def register():
     with open(os.path.join(USERS_FILE_PATH), 'r') as f:
@@ -48,43 +46,78 @@ def login():
         print("Incorrect password. Please try again.")
 
 def log_history(username, message):
+    if os.path.isfile(HISTORY_FILE_PATH) == False or os.stat(HISTORY_FILE_PATH).st_size == 0:
+        with open(os.path.join(HISTORY_FILE_PATH), 'a+') as file:
+            file.write("date, username, message\n")
     now = datetime.datetime.now()
     new_log = str(now) + ", " + username + ", " + message + "\n"
     with open(os.path.join(HISTORY_FILE_PATH),'a+') as file:
         file.write(new_log)
 
+def check_if_number_is_float(number):
+    try:
+        float(number)
+        return True
+    except ValueError:
+        return False
+
 def calc_without_login(username):
-    x = int(input('Enter first number: '))
-    operation = input('Enter operation: ')
-    y = int(input('Enter second number: '))
     calculator = {
         '+': lambda x, y: x + y,
         '-': lambda x, y: x - y,
         '*': lambda x, y: x * y,
         '/': lambda x, y: x / y,
     }
-    if operation in calculator.keys():
-        result = calculator[operation](x, y)
-        if username != None:
-            message = str(x) + operation + str(y) + '=' + str(result)
-            log_history (username, message)
-        return result
+    while True:
+        x = input('Enter first number: ')
+        if check_if_number_is_float(x):
+            operation = input('Enter operation in {}: '.format(list(calculator.keys())))
+            if operation in calculator.keys():
+                y = input('Enter second number: ')
+                if check_if_number_is_float(y):
+                    x = float(x)
+                    y = float(y)
+                    try:
+                        result = calculator[operation](x, y)
+                        if username is not None:
+                            message = str(x) + operation + str(y) + '=' + str(result)
+                            log_history(username, message)
+                        return result
+                    except ZeroDivisionError:
+                        print('Zero division error.')
+                        return 'Zero division error.'
+                else:
+                    print('Invalid number. It must be float. Try again. ')
+            else:
+                print('Incorrect operation. Operation must be in {}'.format(list(calculator.keys())))
+        else:
+            print('Invalid number. It must be float. Try again. ')
     
 def calc_with_login(username):
-    operation = input('Enter operation: ')
-    x = int(input('Enter number: '))
     calculator = {
         'sin': lambda x: math.sin(x),
         'cos': lambda x: math.cos(x),
         'tan': lambda x: math.tan(x),
         'cot': lambda x: math.tan(x)**(-1),
     }
-    if operation in calculator.keys():
-        result = calculator[operation](x)
-        message = operation + '(' + str(x) + ')' + '=' + str(result)
-        log_history(username, message)
-        return result
 
+    while True:
+        operation = input('Enter operation between {}: '.format(list(calculator.keys())))
+        if operation in calculator.keys():
+            x = float(input('Enter number: '))
+            if check_if_number_is_float(x):
+                try:
+                    result = calculator[operation](x)
+                    message = operation + '(' + str(x) + ')' + '=' + str(result)
+                    log_history(username, message)
+                    return result
+                except ZeroDivisionError:
+                    print('Zero division error.')
+                    return 'Zero division error.'
+            else:
+                print('Invalid number. It must be float. Try again. ')
+        else:
+            print('Incorrect operation. Operation must be in {}'.format(list(calculator.keys())))
 
 
 try:
@@ -101,7 +134,7 @@ while True:
         print("No users found. Please register a new account.")
         register()
     else:
-        if logged_user != None:
+        if logged_user is not None:
             print(f"You are currently logged in as: {logged_user}\n")
             options = ['Calculate arithmetic', 'Calculate trigonometric', 'Logout']
         else:
