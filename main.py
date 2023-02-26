6a92262355e4ae9d7072d9810fccedefe2a07d25
sviatoslavhludyk <sv.hludyk@gmail.com>
2023-02-26 10:39:53 +0200

    update

--- main.py
+++ main.py
@@ -3,17 +3,14 @@ import os
 import math
 import datetime
 
-# Define the file names where the user data will be stored
 USERS_FILE_PATH = 'users.json'
 HISTORY_FILE_PATH = 'history.log'
 
 logged_user = None
-clear = lambda: os.system('clear')
+#clear = lambda: os.system('clear')
 
 
 def register():
-    """Allows the user to register a new account."""
-    user_data = {}
     with open(os.path.join(USERS_FILE_PATH), 'r') as f:
         user_data = json.load(f)
     while True:
@@ -23,15 +20,14 @@ def register():
         else:
             break
     password = input("Enter a password: ")
-    # Add the new user to the user data dictionary
     user_data[username] = {'password': password}
-    # Save the updated user data to the JSON file
     with open(os.path.join(USERS_FILE_PATH), 'w+') as f:
         json.dump(user_data, f)
     print("Account created successfully.")    
 
 def login():
-    """Allows the user to log in to an existing account."""
+    with open(os.path.join(USERS_FILE_PATH), 'r') as f:
+        user_data = json.load(f)
     while True:
         username = input("Enter your username: ")
         if username in user_data:
@@ -45,7 +41,6 @@ def login():
                 return
 
     password = input("Enter your password: ")
-    # Check if the provided password matches the stored password for the given username
     if password == user_data[username]['password']:
         print("Login successful!")
         return username
@@ -53,7 +48,6 @@ def login():
         print("Incorrect password. Please try again.")
 
 def log_history(username, message):
-    """Allows the logged in user to save calc history."""
     now = datetime.datetime.now()
     new_log = str(now) + ", " + username + ", " + message + "\n"
     with open(os.path.join(HISTORY_FILE_PATH),'a+') as file:
@@ -88,23 +82,20 @@ def calc_with_login(username):
     if operation in calculator.keys():
         result = calculator[operation](x)
         message = operation + '(' + str(x) + ')' + '=' + str(result)
-        log_history (username, message)
+        log_history(username, message)
         return result
 
 
 
 try:
-    # Load existing user data from the JSON file
     with open(os.path.join(USERS_FILE_PATH), 'r') as f:
         user_data = json.load(f)
 except Exception:
-    # If the file doesn't exist, create it and initialize an empty dictionary
     with open(os.path.join(USERS_FILE_PATH), 'w') as f:
         user_data = {}
         json.dump(user_data, f)
 
 
-# Main program loop
 while True:
     if os.stat(os.path.join(USERS_FILE_PATH)).st_size == 0:
         print("No users found. Please register a new account.")
@@ -135,13 +126,10 @@ while True:
         elif selected_option == 'Login':
             logged_user = login()
         elif selected_option == 'Calculate arithmetic':
-            clear()
             print("Result: ", calc_without_login(logged_user))
         elif selected_option == 'Calculate trigonometric':
-            clear()
-            print("Result: ", calc_with_login(logged_user))                              
+            print("Result: ", calc_with_login(logged_user))
         elif selected_option == 'Logout':
-            # Clear the user data to log out the current user
             user_data.clear()
             logged_user = None
             print("Logout successful!\n")
